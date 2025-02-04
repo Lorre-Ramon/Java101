ea788d6925c354ff6356619e740c5cc37b03b232
Eames Lionel <bxzjshi@126.com>
2025-02-03 23:07:09 -0600

    init repo

---
+++ src/CreateVariable.java
@@ -0,0 +1,11 @@
+public class CreateVariable {
+    public static void main(String[] args){
+        String message; // declare that variable `message` is a String type variable
+        int hour, min;
+        int sec = 59;
+
+        message = "Hello World!"; // assign a value to the variable
+        hour = 23;
+        min = 59;
+    }
+}
---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+public class Hello { // define a name called `Hello`
+
+    public static void main(String[] args) { // method: A named sequence of statements
+        //generate some simple output
+        System.out.println("Hello World!"); // a print line statement, and every statement ends with a ;
+        System.out.println("Howdy y'all");
+        System.out.print("Adios, "); // do not start a new line
+        System.out.println("Cruel World.");
+    } // end main function definition
+
+} // end class definition
